89df4b76349e23162bcc5f22319f4d71457cd6f8
blockjjam <blockjjam99@gmail.com>
2022-03-06 17:33:25 +0900

    feat: FenwickTree
    SegmentTree의 메모리 절약과 업데이트가 자주 일어날 때 유리한 자료구조인 FenwickTree에 대한 학습과 구현

---
+++ src/data_structure/custom/FenwickTree.java
@@ -0,0 +1,23 @@
+package data_structure.custom;
+
+public class FenwickTree {
+    public static long[] tree;
+
+    public static void update(int idx, long diffValue){
+        while(idx < tree.length){
+            tree[idx] += diffValue;
+            idx += (idx & -idx);
+        }
+    }
+
+
+    public static long getSumToIndexRangeValue(int idx){
+        long result = 0;
+        while(idx > 0){
+            result += tree[idx];
+            idx -= (idx & -idx);
+        }
+
+        return result;
+    }
+}
