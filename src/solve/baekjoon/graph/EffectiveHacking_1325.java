1a9e4c06ffdf340937fce8e1069271139d3fd0fb
blockjjam <blockjjam99@gmail.com>
2022-05-10 23:59:59 +0900

    feat: 효율적인 해킹 boj_1325 풀이

--- src/solve/baekjoon/graph/EffectiveHacking_1325.java
+++ src/solve/baekjoon/graph/EffectiveHacking_1325.java
@@ -1,135 +1,60 @@
 package solve.baekjoon.graph;
 
+
 import java.io.*;
 import java.util.*;
 
 public class EffectiveHacking_1325 {
 
-    static int N;
-    static int M;
-    static PriorityQueue<Pair> pq;
+    static int N, M ,ans;
+    static ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[10001];
+    static boolean[] visited = new boolean[10001];
+    static int[] answer = new int[10001];
 
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        pq = new PriorityQueue<>();
 
-        Graph graph = new Graph(N);
+        for(int i=1; i<=N; i++) adj[i] = new ArrayList<>();
 
-        int s, d;
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
-            d = Integer.parseInt(st.nextToken()) - 1; // vertex N을 0번부터 넣었기 때문
-            s = Integer.parseInt(st.nextToken()) - 1;
-
-            graph.addEdge(graph.getVertex(s), graph.getVertex(d));
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            adj[x].add(y);
         }
 
-        for(int i=0; i<N; i++){
-            Vertex target = graph.getVertex(i);
-            graph.bfs(target);
-            graph.cleanVisited();
-
+        ans = 0;
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++) visited[j] = false;
+            dfs(i);
         }
 
-        int maxHacked = -1;
-        while(!pq.isEmpty()){
-            Pair candidate = pq.poll();
-            if(maxHacked == -1)
-                maxHacked = candidate.hackCnt;
-
-            if(candidate.hackCnt != maxHacked)
-                break;
-
-            bw.write((candidate.v+1) +" ");
+        for(int i=1; i<=N; i++){
+            ans = Math.max(ans, answer[i]);
         }
 
-        bw.flush();
-        bw.close();
+        for(int i=1; i<=N; i++){
+            if(ans == answer[i]){
+                System.out.print(i +" ");
+            }
+        }
+
     }
 
-    static class Vertex{
-        int data;
-        boolean visited;
-        boolean noSearch;
-        List<Vertex> adList = new LinkedList<>();
+    public static void dfs(int num){
+        visited[num] = true;
 
-        public Vertex(int data){
-            this.data = data;
-            this.visited = false;
-            this.noSearch = false;
-        }
-    }
-
-    static class Graph{
-        private List<Vertex> vertexList = new LinkedList<>();
-        public Graph(int v){
-            for(int i=0; i<v; i++){
-                vertexList.add(new Vertex(i));
-            }
-        }
-
-        public Vertex getVertex(int v){
-            return vertexList.get(v);
-        }
-
-        public void addEdge(Vertex s, Vertex d){
-            s.adList.add(d);
-        }
-
-        public void bfs(Vertex v){
-            Queue<Vertex> queue = new LinkedList<>();
-            Queue<Vertex> sameHackPowerVertexQueue = new LinkedList<>();
-            v.visited = true;
-            v.noSearch =true;
-            queue.offer(v);
-            sameHackPowerVertexQueue.offer(v);
-
-            int result = 0;
-            while(!queue.isEmpty()){
-                Vertex target = queue.poll();
-                if(target.noSearch){
-                    sameHackPowerVertexQueue.add(target);
-                }
-                for(Vertex adjV : target.adList){
-                    if( !adjV.visited){
-                        adjV.visited = true;
-                        adjV.noSearch = true;
-                        queue.offer(adjV);
-                        result++;
-                    }
-                }
-            }
-            while(!sameHackPowerVertexQueue.isEmpty()){
-                Vertex sameHackVertex = sameHackPowerVertexQueue.poll();
-                pq.add(new Pair(result, sameHackVertex.data));
-            }
-            pq.add(new Pair(result, v.data));
-        }
-
-        public void cleanVisited(){
-            for(Vertex v : vertexList){
-                v.visited = false;
+        for(int next: adj[num]){
+            if(!visited[next]){
+                answer[next]++;
+                dfs(next);
             }
         }
     }
 
-    static class Pair implements Comparable<Pair>{
-        int hackCnt;
-        int v;
-
-        public Pair(int hackCnt, int v){
-            this.hackCnt = hackCnt;
-            this.v = v;
-        }
-
-        @Override
-        public int compareTo(Pair pair) {
-            return pair.hackCnt != hackCnt ? pair.hackCnt - hackCnt : v - pair.v;
-        }
-    }
 }
