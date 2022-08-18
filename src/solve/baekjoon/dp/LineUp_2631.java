4336fdc9b01ba7abf290a679ec2509c9955ee5f4
blockjjam <blockjjam99@gmail.com>
2022-08-18 09:03:58 +0900

    feat: LIS 풀이 2가지 방식 dp, 이분탐색 줄세우기 boj_2631 PS

--- src/solve/baekjoon/dp/LineUp_2631.java
+++ src/solve/baekjoon/dp/LineUp_2631.java
@@ -5,90 +5,70 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/**
+ * LIS && DP
+ */
 public class LineUp_2631 {
-    static int N;
-    static int[] numArr;
-    static boolean[] visited;
-    static List<Integer> students = new ArrayList<>();
-    static int result;
+
 
     public static void main(String[] args) {
         FastReader rd = new FastReader();
 
-        N = rd.nextInt();
-        numArr = new int[N];
-        visited = new boolean[N];
-        result = -1;
-//        for(int i=0; i<N; i++){
-//            numArr[i] = i+1;
-//            students.add(rd.nextInt());
+        // O(N^2)
+//        int n = rd.nextInt();
+//        int[] arr = new int[n];
+//        List<Integer> list = new ArrayList<>();
+//
+//        for(int i=0; i<n; i++){
+//            arr[i] = rd.nextInt();
 //        }
-        for(int i=N-1; i>=0; i--){
-            numArr[i] = i+1;
-            students.add(i+1);
-        }
-
-        for(int i=1; i< N+1; i++){
-            if(result == -1){
-                findMinChangeCount(0, i);
-            }
-        }
-
-        System.out.println(result);
-
-    }
-
-    static void findMinChangeCount(int start, int round){
-        if(round == 0){
-            // 해당 조합에서 순서변경 후, 순서가 올바르게 만들어지는지 확인
-            List<Integer> copiedStudents = new ArrayList<>();
-            copiedStudents.addAll(students);
-//            Collections.copy(copiedStudents, students);
-            int total = 0;
-            for(int i=0; i<visited.length; i++){
-                if(visited[i]) {
-                    // 방문한 자리의 숫자의 자리를 꺼내서 순서대로 맞는 자리를 찾도록 하는 함수 호출
-//                    System.out.print(i + " ");
-                    findCollectLocation(copiedStudents, i+1);
-                    ++total;
-                }
-            }
-//            System.out.println();
-//            System.out.println(copiedStudents);
-            int prev = 0;
-            boolean found = true;
-            for(int target : copiedStudents){
-                if(prev > target){
-                    found = false;
-                    break;
-                }
-                prev = target;
-            }
-            if(found){
-                result = total;
-                return;
-            }
-//            for(int i=0; i < copiedStudents.size(); i++){
-//                int target = copiedStudents.get(i);
-//                if(target != i+1) break;
-//                if(i == copiedStudents.size() -1) {
-//                    result = total;
+//
+//        for(int num : arr){
+//            if(list.size() == 0 || list.get(list.size() -1 ) < num){
+//                list.add(num);
+//            }else{
+//                int i = 0;
+//                int j  = list.size() -1;
+//
+//                while(i < j){
+//                    int mid = (i + j)/2;
+//
+//                    if(list.get(mid) < num){
+//                        i = mid + 1;
+//                    }else{
+//                        j = mid;
+//                    }
 //                }
+//                list.set(j, num);
 //            }
+//        }
+//        System.out.println(n - list.size());
+
+        // O(N*logN)
+        int n = rd.nextInt();
+        int[] dp = new int[n];
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = rd.nextInt();
         }
 
-        for(int i = start; i< N; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                findMinChangeCount(i+1, round -1);
-                visited[i] = false;
+        dp[0] = 1;
+        for(int i= 1; i< n; i++){
+            dp[i] = 1;
+            for(int j= 0; j < i; j++){
+                if(arr[j] < arr[i] && dp[j] +1 > dp[i]){
+                    dp[i] = dp[j] + 1;
+                }
             }
         }
-    }
 
-    static void findCollectLocation(List<Integer> list, int target){
-        int targetIndex = list.indexOf(target);
-        list.remove(targetIndex);
+        int ans = 0;
+        for( int i=1; i<n; i++){
+            ans = Math.max(ans, dp[i]);
+        }
+
+        System.out.println(n - ans);
     }
 
     static class FastReader {
