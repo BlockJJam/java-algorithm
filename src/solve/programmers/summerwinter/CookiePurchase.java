6855a8c0ee277f023fdfeed60b8f804f36bd2328
blockjjam <blockjjam99@gmail.com>
2022-09-23 10:29:11 +0900

    feat: Summer/Winter 쿠키 구입 PS - cookie배열 모든 원소를 각각 m으로 설정한다. - m부터 왼쪽 cookie원소를 체크하고, m부터 오른쪽 cookie원소를 체크해서 lSum, rSum을 만든다 - answer = Max(answer, lSum) if lSum == rSum

--- src/solve/programmers/summerwinter/CookiePurchase.java
+++ src/solve/programmers/summerwinter/CookiePurchase.java
@@ -4,112 +4,47 @@ public class CookiePurchase {
     public static int solution(int[] cookie) {
         int answer = -1;
 
-        // 1개 일 때와 2개 일때 전처리
-        if(cookie.length == 1){
-            return -1;
-        }else if(cookie.length == 2){
-            int ans = cookie[0] == cookie[1] ? cookie[0] : -1;
-            return ans;
-        }
+        int max = 0;
+        for (int i = 0; i < cookie.length-1; i++) {
+            // 모든 cookie배열 위치에 m을 적용한다.
+            int prev = i;
+            int next = i+1;
 
-        // 이분법과 세그먼트 트리를 사용할 예정
-        SegmentTree st = new SegmentTree(cookie.length);
-        for(int i= 0; i<cookie.length; i++){
-            st.updateTree(i+1, cookie[i]);
-//            System.out.println("i = " + i);
-        }
+            int lSum = cookie[prev];
+            int rSum = cookie[next];
+            while(true){
+                // lSum과 rSum을 비교한다.
+                if(lSum == rSum){
+                    // lSum과 rSum이 같으면, max중에 더 큰 값을 지정하고 prev-- , next++ 를 체크한다.
+                    max = Math.max(lSum, max);
+                    if(--prev == -1 || ++next == cookie.length){
+                        break;
+                    }
 
-        int pivot = findPivot(1, cookie.length, st);
-
-        int noDiff = isNoDiff(pivot, cookie.length, st);
-        if(noDiff == 0){
-            return st.getSum(pivot);
-        }else if(noDiff == -1){
-            pivot++;
-        }
-        // l과 r을 조절하지 않은 상태에서 m+1->r까지 합 - l->m합 > 0 중에 최소인 m을 pivot으로 구함
-
-        int l = 1;
-        for(int m = pivot; m < cookie.length; m++){
-            for(int i = l; i < m; i++){
-                // l 첫번째 바구니부터 제거해서, m까지 합이 m+1 - r까지 합보다 작아지면 break;
-                int sumDiff = (st.getSum(cookie.length) - st.getSum(m)) - (st.getSum(m) - st.getSum(i));
-//                System.out.println("sumDiff = " + sumDiff);
-                if(sumDiff <= 0) {
-//                    System.out.println("i = " + i + " m = " + m);
-                    l = i;
-                    break;
+                    lSum += cookie[prev];
+                    rSum += cookie[next];
+                }else if(lSum < rSum){
+                    // lSum이 더 작으면, prev--를 체크하고, lSum을 업데이트한다.
+                    if(--prev == -1){
+                        break;
+                    }
+                    lSum += cookie[prev];
+                }else{
+                    // rSum이 더 작으면, next++을 체크하고, rSum을 업데이트한다.
+                    if(++next == cookie.length){
+                        break;
+                    }
+                    rSum += cookie[next];
                 }
             }
-
-            // m -l 과 m+1 - r 을 비교 0이면 리턴, 아니면 continue;
-            if((st.getSum(cookie.length) - st.getSum(m)) - (st.getSum(m) - st.getSum(l)) == 0){
-                return st.getSum(cookie.length) - st.getSum(m);
-            }
         }
 
-        return 0;
-    }
-
-    static int isNoDiff(int m, int r, SegmentTree st){
-        int left = st.getSum(m);
-        int right = st.getSum(r) - st.getSum(m);
-
-//        System.out.println("left = " + left + " right = " + right);
-
-        // -1 이면 왼쪽이 더 적음, 0이면 과자 수 같음, 1이면 오른쪽이 더 적음
-        return Integer.compare(left, right);
-
-    }
-
-    static int findPivot(int l, int r, SegmentTree st){
-        // l ~ m 까지 합과 m+1 ~ r 까지 합의 차가 가장 작을 때의 pivot을 꺼낸다.
-        int m = 0;
-        while(l < r){
-            m = (l+r)/2;
-//            System.out.println("m = " + m);
-            int lSum = st.getSum(m);
-            int rSum = st.getSum(r) - st.getSum(m);
-            if(lSum < rSum ){
-                l = m+1;
-            }else if(lSum > rSum){
-                r = m;
-            }else{
-                break;
-            }
-        }
-
-        return m;
-    }
-
-    static class SegmentTree{
-        private int[] tree;
-
-        SegmentTree(int n){
-            tree = new int[n+1];
-        }
-
-        public void updateTree(int idx, int value){
-            while(idx <= tree.length){
-//                System.out.println("idx = " + idx);
-                tree[idx] += value;
-                idx += (idx & -idx);
-            }
-        }
-
-        public int getSum(int idx){
-            int sum = 0;
-            while(idx > 0){
-                sum += tree[idx];
-                idx -= (idx & -idx);
-            }
-
-            return sum;
-        }
+        return max;
     }
 
     public static void main(String[] args) {
-        int ret = solution(new int[]{1,1,2,3});
+//        int ret = solution(new int[]{1,1,2,3});
+        int ret = solution(new int[]{1, 1, 1, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5,});
 //        int ret = solution(new int[]{1,2,4,5});
 
         System.out.println("ret = " + ret);
